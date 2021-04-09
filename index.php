806ebea66f08e56e8f10cf9e45cfced09475830f
wtsaygers1 <wtsaygers1@gmail.com>
2021-04-09 08:06:22 -0400

    demo check

--- index.php
+++ index.php
@@ -22,8 +22,8 @@
           <ul>
             <li><a href="/api.php?route=getAllHeroes" target="_blank">Get All Heroes</a></li>
             <li><a href="/api.php?route=getHeroById&hero_id=5" target="_blank">Get Hero By ID</a></li>
-            <li><a href="/api.php?route=addHeroToTable&name=Spider Man&about_me=''&biography=''" target="_blank">Add Hero</a></li>
-            <li><a href="/api.php?route=deleteHeroFromTable&id=8&name=''&about_me=''&biography=''" target="_blank">Delete Hero</a></li>
+            <li><a href="/api.php?route=addHeroToTable&name=Capt Marvel&about_me=''&biography=''" target="_blank">Add Hero</a></li>
+            <li><a href="/api.php?route=deleteHeroFromTable&id=7&name=''&about_me=''&biography=''" target="_blank">Delete Hero</a></li>
             <li><a href="/api.php?route=updateHeroAbility&id=5&ability='Frost Breath'" target="_blank">Update Abilities</a></li>
           </ul>
         </div>
